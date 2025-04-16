a8f0eb1a5ca715bed957a2000513d6696d83adae
Abderraouf <jabli.abderraouf@gmail.com>
2025-04-16 02:02:52 +0100

    ajout place model

---
+++ model/place.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const placeSchema = mongoose.Schema({
+  numero : {
+    type : String,
+    unique : true,
+    validate : {
+       validator: async function(numero){
+      const existing = await this.constructor.findOne({
+        numero : numero,
+        parking : this.parking
+      });
+      return !exixting
+    },
+    message : 'Le numero de place doit étre unique dans ce parking'
+  }
+  },
+  
+  disponible : {type : boolean ,default : true},
+  type : { type:String , enum : ['standard', 'handicape','electrique'], default : 'standard'},
+  parking : {type : mongoose.Schema.Types.ObjectId , ref : Parking, required : true}
+
+})
