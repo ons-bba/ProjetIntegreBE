79a8161036a6dff0bfacea5db2426a0568a5dabb
Abderraouf <jabli.abderraouf@gmail.com>
2025-04-22 14:59:25 +0100

    envois parking en Bulk

--- controller/parkingController.js
+++ controller/parkingController.js
@@ -13,8 +13,8 @@ const parkingController = {
                 return res.status(400).json({ error: "Les coordonnées de localisation sont requises" });
             }
     
-            const { longitude, latitude, ...rest } = req.body.localisation.coordinates
-                ? {
+            const { longitude, latitude, ...rest } = req.body.localisation.coordinates?
+                 {
                     longitude: req.body.localisation.coordinates[0],
                     latitude: req.body.localisation.coordinates[1],
                     ...req.body
@@ -102,17 +102,89 @@ const parkingController = {
     },
 
     deleteParking: async (req, res) => {
+        
+        const {id} = req.params
 
         try {
-            const deleted = await Parking.findByIdAndDelete(req.params.id);
-            if (!deleted) return res.status(404).json({ message: 'Parking non trouvé' });
+
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                throw new Error("id n'est pas un objectId")
+            }
+            const deleted = await Parking.findByIdAndDelete(id);
+
+            if (!deleted) {
+
+                throw new Error("Erreur serveur")
+            }
             res.status(200).json({ message: 'Parking supprimé avec succès' });
         } catch (err) {
-            res.status(500).json({ error: err.message });
+            res.status(500).json({ 
+                success : false,
+                message:"Erreur serveur lors de la mise a jour de parking",
+                error: err.message });
         }
 
 
 
+    },
+    importParkingFromJson: async (req,res)=>{
+        try{
+            if(!req.body || !Array.isArray(req.body)){
+                return res.status(400).json({
+                    success : false,
+                    message : "le corps de la requette doit contenir un tableau de parking"
+                });
+            }
+            // valider et transformer chaque parking 
+            const parkingToInsert = req.body.map(parkingData=>{
+                //validation basique
+                if(!parkingData.nom || !parkingData.placesTotal || ! parkingData.localisation?.coordinates){
+                    throw new Error(`Parking invalid: ${JSON.stringify(parkingData)}`)
+                }
+                return {
+                    nom : parkingData.nom,
+                    statut:parkingData.statut || 'OUVERT',
+                    placesTotal:parkingData.placesTotal,
+                    placesDisponible: parkingData.placesDisponible || parkingData.placesTotal,
+                    localisation: {
+                        type: 'Point',
+                        coordinates : [
+                            parseFloat(parkingData.localisation.coordinates[0]),
+                            parseFloat(parkingData.localisation.coordinates[1])
+                        ]
+                    },
+                    prestations: parkingData.prestations || [],
+                    horaires: parkingData.horaires || {
+                        ouverture: '08:00',
+                        fermeture: '20:00'
+                    }
+                }
+            });
+            // Insert en masse 
+            const result = await Parking.insertMany(parkingToInsert,{ordered : false})
+            res.status(201).json({
+                sucess : true,
+                message: `${result.length} parking créés avec succès`,
+                data : result
+            })
+
+        }catch(err){
+            // Gestion erreur en Bulk insert 
+            if(err.name ==='BulkWriteError'){
+                const successCount = err.result.result.nInserted;
+                return res.status(207).json({
+                    success: false,
+                    message : `opération partiellement réussie(${successCount} parking créés)`,
+                    error:  err.writeErrors.map(e=>e.errmsg)
+                });
+            }
+
+        }
+        res.status(500).json({
+            success: false,
+            message: "Erreur lors de l'import des parkings",
+            error:err.message
+        })
     }
 }
 
--- routes/parking.js
+++ routes/parking.js
@@ -10,7 +10,8 @@ router.post('/',parkingController.createParking);
 router.get('/',parkingController.getAllParkings);
 router.get('/:id',parkingController.getParkingById);
 router.put('/:id',parkingController.updateParking)
-router.delete('/:id',parkingController.deleteParking)
+router.delete('/:id',parkingController.deleteParking);
+router.post('/upload-json',parkingController.importParkingFromJson);
 
 
 
